69afc9ad6707d1039107970c02b28e7fe010a635
Dharmendra Awasthi <dharmendrakawasthi@gmail.com>
2021-02-15 19:36:36 +0530

    Initialized Tests

---
+++ repository/src/test/java/com/techendear/ebill/UserRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.techendear.ebill;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserRepositoryTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
